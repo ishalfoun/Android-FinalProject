548faf55c1b2ea092f132573e0da3268aa49f286
jacobr98 <jacob.riendeau1998@gmail.com>
2017-11-20 14:53:10 -0500

    main layout

--- app/src/main/java/dawson/dawsondangerousclub/MainActivity.java
+++ app/src/main/java/dawson/dawsondangerousclub/MainActivity.java
@@ -1,5 +1,6 @@
 package dawson.dawsondangerousclub;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -10,4 +11,38 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void launchCancellations(){
+        Intent cancellations = new Intent(MainActivity.this,CancelledClassesActivity.class);
+        startActivity(cancellations);
+    }
+
+    public void launchTeachers(){
+
+    }
+
+    public void launchCalendar(){
+
+    }
+
+    public void launchNotes(){
+
+    }
+
+    public void launchWeather(){
+
+    }
+
+    public void launchAcademicCalendar(){
+
+    }
+
+    public void launchAbout(){
+
+    }
+
+    public void launchDawson(){
+
+    }
+
 }
