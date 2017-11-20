d0c5a05af019ab1a5125f8d7758838bde4e84026
Theodore Acco-Thomas <theodoreaccothomas@gmail.com>
2017-11-20 14:39:07 -0500

    Add menu items and their activities

--- app/src/main/java/dawson/dawsondangerousclub/SettingsActivity.java
+++ app/src/main/java/dawson/dawsondangerousclub/SettingsActivity.java
@@ -1,24 +1,30 @@
 package dawson.dawsondangerousclub;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class SettingsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_settings);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the main; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
+
+        // remove about because we don't need it here
+        menu.removeItem(R.id.settings);
+
         return true;
     }
 
@@ -36,13 +42,6 @@ public class MainActivity extends AppCompatActivity {
             case R.id.dawson:
                 Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.dawsoncollege.qc.ca/computer-science-technology/"));
                 startActivity(i);
-                return true;
-            //Open last viewed quote
-            case R.id.settings:
-                //open settings activity
-                Intent openSettings = new Intent(getApplicationContext(),
-                        SettingsActivity.class);
-                startActivity(openSettings);
                 return true;
             default:
                 return false;
