4d64eb25bb32d94acfad44792a3dd2a9555bb9bb
Theodore Acco-Thomas <theodoreaccothomas@gmail.com>
2017-11-23 02:29:32 -0500

    Fix academic calendar

--- app/src/main/java/dawson/dawsondangerousclub/AcademicCalendarActivity.java
+++ app/src/main/java/dawson/dawsondangerousclub/AcademicCalendarActivity.java
@@ -10,6 +10,7 @@ import android.view.View;
 import android.webkit.WebView;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
 import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.Toast;
@@ -19,6 +20,7 @@ public class AcademicCalendarActivity extends AppCompatActivity {
     private WebView webView;
     Spinner spinner;
     RadioGroup radioGroup;
+    boolean firstStart = true;
 
     public static final String FALL_2014 = "https://www.dawsoncollege.qc.ca/registrar/fall-2014-day-division/";
     public static final String FALL_2015 = "https://www.dawsoncollege.qc.ca/registrar/fall-2015-day-division/";
@@ -36,8 +38,8 @@ public class AcademicCalendarActivity extends AppCompatActivity {
         setContentView(R.layout.activity_academic_calendar);
 
         webView = (WebView) findViewById(R.id.academicCalendarWV);
-        spinner = (Spinner)findViewById(R.id.academicYrSpinner);
-        radioGroup = (RadioGroup)findViewById(R.id.radioGroup) ;
+        spinner = (Spinner) findViewById(R.id.academicYrSpinner);
+        radioGroup = (RadioGroup) findViewById(R.id.radioGroup);
 
         String[] items = new String[]{"2014", "2015", "2016", "2017"};
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.spinner_item, items);
@@ -46,62 +48,66 @@ public class AcademicCalendarActivity extends AppCompatActivity {
         //set default values
         spinner.setSelection(3);
         radioGroup.check(R.id.fall);
-        //webView.loadUrl(FALL_2017);
+        webView.loadUrl(FALL_2017);
 
 
         spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
 
-                //webView.loadUrl("https://stackoverflow.com/questions/33725240/change-webview-url-on-button-tap");
 
+                if (firstStart) {
+                    //default setup, do nothing
+                    firstStart = false;
+                } else {
 
-                String season;
-                Toast.makeText(getApplicationContext(), "Selection is " + position, Toast.LENGTH_LONG).show();
+                    //default season
+                    boolean fallSeason = true;
 
+                    //determine which season is selected
+                    if (radioGroup.getCheckedRadioButtonId() == R.id.fall) {
+                        fallSeason = true;
+                    } else {
+                        fallSeason = false;
+                    }
 
-                //determine which season is selected
-                if(radioGroup.getCheckedRadioButtonId()!=R.id.fall){
-                    season = "fall";
-                }else{
-                    season = "winter";
-                }
-                //open webpage page based year selected
-                switch(position){
-                    //2014
-                    case 0:
-                        switch(season){
-                            case "fall": webView.loadUrl(FALL_2014);
-                                break;
-                            case "winter": webView.loadUrl(WINTER_2014);
-                                break;
-                        }
+                    //open web page page based year selected
+                    switch (position) {
+                        //2014
+                        case 0:
+                            if (fallSeason) {
+                                webView.loadUrl(FALL_2014);
+                            } else {
+                                webView.loadUrl(WINTER_2014);
+                            }
+                            break;
                         //2015
-                    case 1:
-                        switch(season){
-                            case "fall": webView.loadUrl(FALL_2015);
-                                break;
-                            case "winter": webView.loadUrl(WINTER_2015);
-                                break;
-                        }
+                        case 1:
+                            if (fallSeason) {
+                                webView.loadUrl(FALL_2015);
+                            } else {
+                                webView.loadUrl(WINTER_2015);
+                            }
+                            break;
                         //2016
-                    case 2:
-                        switch(season){
-                            case "fall": webView.loadUrl(FALL_2016);
-                                break;
-                            case "winter": webView.loadUrl(WINTER_2016);
-                                break;
-                        }
+                        case 2:
+                            if (fallSeason) {
+                                webView.loadUrl(FALL_2016);
+                            } else {
+                                webView.loadUrl(WINTER_2016);
+                            }
+                            break;
                         //2017
-                    case 3:
-                        switch(season){
-                            case "fall": webView.loadUrl(FALL_2017);
-                                break;
-                            case "winter": webView.loadUrl(WINTER_2017);
-                                break;
-                        }
-                }
+                        case 3:
+                            if (fallSeason) {
+                                webView.loadUrl(FALL_2017);
+                            } else {
+                                webView.loadUrl(WINTER_2017);
+                            }
+                            break;
+                    }
 
+                }
             }
 
             @Override
@@ -111,7 +117,58 @@ public class AcademicCalendarActivity extends AppCompatActivity {
 
         });
 
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
 
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+
+                String year = spinner.getSelectedItem().toString();
+
+                //default season
+                boolean fallSeason = true;
+
+                if (checkedId == R.id.fall) {fallSeason = true;} else{fallSeason = false;}
+
+                //open web page page based year selected
+                switch (year) {
+                    //2014
+                    case "2014":
+                        if (fallSeason) {
+                            webView.loadUrl(FALL_2014);
+                        } else {
+                            webView.loadUrl(WINTER_2014);
+                        }
+                        break;
+                    //2015
+                    case "2015":
+                        if (fallSeason) {
+                            webView.loadUrl(FALL_2015);
+                        } else {
+                            webView.loadUrl(WINTER_2015);
+                        }
+                        break;
+                    //2016
+                    case "2016":
+                        if (fallSeason) {
+                            webView.loadUrl(FALL_2016);
+                        } else {
+                            webView.loadUrl(WINTER_2016);
+                        }
+                        break;
+                    //2017
+                    case "2017":
+                        if (fallSeason) {
+                            webView.loadUrl(FALL_2017);
+                        } else {
+                            webView.loadUrl(WINTER_2017);
+                        }
+                        break;
+                }
+
+            }
+
+
+        });
     }
 
     @Override
