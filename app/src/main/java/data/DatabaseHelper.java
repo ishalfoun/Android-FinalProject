1d213c78e0c76d511a79e4f40f468298249e457b
jacobr98 <jacob.riendeau1998@gmail.com>
2017-11-24 18:04:13 -0500

    forgot to push to remote

--- app/src/main/java/data/DatabaseHelper.java
+++
@@ -1,8 +0,0 @@
-package data;
-
-/**
- * Created by 1537385 on 11/20/2017.
- */
-
-public class DatabaseHelper {
-}
--- app/src/main/java/utilities/ListViewNoteAdapter.java
+++ app/src/main/java/utilities/ListViewNoteAdapter.java
@@ -1,8 +1,68 @@
 package utilities;
 
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import java.util.List;
+
+import dawson.dawsondangerousclub.NotesActivity;
+import dawson.dawsondangerousclub.R;
+import notes.Note;
+import notes.NotesDatabaseHelper;
+
 /**
  * Created by 1537385 on 11/20/2017.
  */
 
-public class ListViewNoteAdapter {
+public class ListViewNoteAdapter extends BaseAdapter {
+    private Context context;
+    private LayoutInflater inflater;
+    private List<Note> elements;
+
+    public ListViewNoteAdapter(Context context, List<Note> elements) {
+        this.context = context;
+        this.elements = elements;
+        this.inflater = LayoutInflater.from(context);
+    }
+
+    @Override
+    public int getCount() {
+        return elements.size();
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return elements.get(i);
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        View rowView = inflater.inflate(R.layout.listelement_notes,null);
+        TextView textView = rowView.findViewById(R.id.noteListTextView);
+        textView.setText(elements.get(i).getNote());
+        ImageButton imageBttn =  (ImageButton) rowView.findViewById(R.id.add_noteImgBttn);
+        //imageBttn.setId(elements.get(i).getId());
+        final int ii = i;
+        imageBttn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NotesDatabaseHelper database = new NotesDatabaseHelper(context);
+                database.deleteNote(elements.get(ii).getId());
+                NotesActivity activity = (NotesActivity)context;
+                activity.onDelete();
+            }
+        });
+        return rowView;
+    }
 }
