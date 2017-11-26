6b75a327322c2c68c9617603ce7b7e9f6545de35
Theodore Acco-Thomas <theodoreaccothomas@gmail.com>
2017-11-26 06:16:54 -0500

    complete weather activity

--- app/src/main/java/dawson/dawsondangerousclub/WeatherActivity.java
+++ app/src/main/java/dawson/dawsondangerousclub/WeatherActivity.java
@@ -37,6 +37,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
@@ -51,9 +52,10 @@ public class WeatherActivity extends AppCompatActivity {
     ListView forecastListView;
     LinearLayout mainLayout;
     InputMethodManager keyboard;
+    TextView uvTV;
 
-    String selectedCity ="montreal";
-    String countryCode ="ca";
+    String selectedCity = "montreal";
+    String countryCode = "ca";
     String latitude;
     String longitude;
 
@@ -72,13 +74,14 @@ public class WeatherActivity extends AppCompatActivity {
 
         cityInput = (EditText) findViewById(R.id.cityET);
         countrySelector = (Spinner) findViewById(R.id.countrySpinner);
-        mainLayout = (LinearLayout)findViewById(R.id.weatherWindow);
+        uvTV = (TextView) findViewById(R.id.uvTV);
+        mainLayout = (LinearLayout) findViewById(R.id.weatherWindow);
         forecastListView = (ListView) findViewById(R.id.forecastLV);
-        keyboard = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+        keyboard = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
 
         getCountriesFromFile();
 
-        //attach countres to spinner
+        //attach countries to spinner
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.spinner_item, countries);
         countrySelector.setAdapter(adapter);
 
@@ -86,7 +89,6 @@ public class WeatherActivity extends AppCompatActivity {
         countrySelector.setSelection(39);
         getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
         forecastListView.setDivider(null);
-
 
         //get default montreal forecast
         getForecast();
@@ -97,37 +99,36 @@ public class WeatherActivity extends AppCompatActivity {
     /**
      * Launches async task to get forecast, checks for network connectivity first.
      */
-    public void getForecast(){
+    public void getForecast() {
         // first check to see if we can get on the network
         ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
-        if (networkInfo != null && networkInfo.isConnected()){
+        if (networkInfo != null && networkInfo.isConnected()) {
             new getForecastAsync().execute();
 
         } else {
             Toast.makeText(getApplicationContext(), "No network connection available.", Toast.LENGTH_LONG).show();
 
         }
-
         //hide keyboard
         keyboard.hideSoftInputFromWindow(mainLayout.getWindowToken(), 0);
     }
 
     /**
      * Button onClick() to get forecast.
+     *
      * @param view
      */
-    public void findForecast(View view){
+    public void findForecast(View view) {
         selectedCity = cityInput.getText().toString().trim();
         countryCode = countrySelector.getSelectedItem().toString().split("\\|")[0].trim();
         getForecast();
-
     }
 
     /**
-     * Read json file with all the countries and their ISO 3166 country codes . Puts them into an arraylist.
+     * Read json file with all the countries and their ISO 3166 country codes . Puts them into an array list.
      */
-    public void getCountriesFromFile(){
+    public void getCountriesFromFile() {
 
         countries = new ArrayList<String>();
 
@@ -137,7 +138,7 @@ public class WeatherActivity extends AppCompatActivity {
 
             JSONArray json = new JSONArray(jsonTxt);
 
-            for(int country=0; country < 249; country++ ){
+            for (int country = 0; country < 249; country++) {
 
                 JSONObject countryData = (JSONObject) json.get(country);
                 String countryCode = countryData.getString("Code");
@@ -145,7 +146,7 @@ public class WeatherActivity extends AppCompatActivity {
                 countries.add(countryCode + "|" + countryName);
             }
 
-        }catch(Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
@@ -153,49 +154,67 @@ public class WeatherActivity extends AppCompatActivity {
     /**
      * Gets the image resource id according to the image code.
      *
-     * @param imageCode
+     * @param imageCode internal weather description code
      * @return image resource id
      */
     private int getForecastImage(String imageCode) {
 
         int imageResourceId = 0;
 
-        switch(imageCode){
-            case "01d": imageResourceId = R.drawable.w01d;
+        switch (imageCode) {
+            case "01d":
+                imageResourceId = R.drawable.w01d;
                 break;
-            case "01n": imageResourceId = R.drawable.w01n;
+            case "01n":
+                imageResourceId = R.drawable.w01n;
                 break;
-            case "02d": imageResourceId = R.drawable.w02d;
+            case "02d":
+                imageResourceId = R.drawable.w02d;
                 break;
-            case "02n": imageResourceId = R.drawable.w02n;
+            case "02n":
+                imageResourceId = R.drawable.w02n;
                 break;
-            case "03d": imageResourceId = R.drawable.w03d;
+            case "03d":
+                imageResourceId = R.drawable.w03d;
                 break;
-            case "03n": imageResourceId = R.drawable.w03n;
+            case "03n":
+                imageResourceId = R.drawable.w03n;
                 break;
-            case "04d": imageResourceId = R.drawable.w04d;
+            case "04d":
+                imageResourceId = R.drawable.w04d;
                 break;
-            case "04n": imageResourceId = R.drawable.w04n;
+            case "04n":
+                imageResourceId = R.drawable.w04n;
                 break;
-            case "09d": imageResourceId = R.drawable.w09d;
+            case "09d":
+                imageResourceId = R.drawable.w09d;
                 break;
-            case "09n": imageResourceId = R.drawable.w09n;
+            case "09n":
+                imageResourceId = R.drawable.w09n;
                 break;
-            case "10d": imageResourceId = R.drawable.w10d;
+            case "10d":
+                imageResourceId = R.drawable.w10d;
                 break;
-            case "10n": imageResourceId = R.drawable.w10n;
+            case "10n":
+                imageResourceId = R.drawable.w10n;
                 break;
-            case "11d": imageResourceId = R.drawable.w11d;
+            case "11d":
+                imageResourceId = R.drawable.w11d;
                 break;
-            case "11n": imageResourceId = R.drawable.w11n;
+            case "11n":
+                imageResourceId = R.drawable.w11n;
                 break;
-            case "13d": imageResourceId = R.drawable.w13d;
+            case "13d":
+                imageResourceId = R.drawable.w13d;
                 break;
-            case "13n": imageResourceId = R.drawable.w13n;
+            case "13n":
+                imageResourceId = R.drawable.w13n;
                 break;
-            case "50d": imageResourceId = R.drawable.w50d;
+            case "50d":
+                imageResourceId = R.drawable.w50d;
                 break;
-            case "50n": imageResourceId = R.drawable.w50n;
+            case "50n":
+                imageResourceId = R.drawable.w50n;
                 break;
 
         }
@@ -229,15 +248,15 @@ public class WeatherActivity extends AppCompatActivity {
 
         protected void onPostExecute(String result) {
 
-            if(result == "Invalid request."){
+            if (result.equalsIgnoreCase("Invalid request.")) {
                 Toast.makeText(getApplicationContext(), "No results found.", Toast.LENGTH_LONG).show();
-            }else {
+            } else {
                 //empty any previous saved forecasts
                 forecastDetails.clear();
                 forecastImages.clear();
 
                 String[] forecastAndLatAndLon = result.split("#");
-                Log.i("latandlong", forecastAndLatAndLon[0] );
+                Log.i("latandlong", forecastAndLatAndLon[0]);
                 String[] latAndLon = forecastAndLatAndLon[0].split("\\?");
                 latitude = latAndLon[0];
                 longitude = latAndLon[1];
@@ -246,22 +265,23 @@ public class WeatherActivity extends AppCompatActivity {
                 String[] allForecasts = forecastAndLatAndLon[1].split("\\|");
                 //Log.i("line numbers", Integer.toString(allForecasts.length));
 
-                for(String forecast: allForecasts){
-                   // Log.i("line", forecast );
+                for (String forecast : allForecasts) {
+                    // Log.i("line", forecast );
 
-                     String[] forecastAndImgArr = forecast.split("@");
-                   // Log.i("line", forecastAndImgArr[0] );
+                    String[] forecastAndImgArr = forecast.split("@");
+                    // Log.i("line", forecastAndImgArr[0] );
                     forecastDetails.add(forecastAndImgArr[0]);
                     forecastImages.add(getForecastImage(forecastAndImgArr[1].trim()));
 
-                     //Log.i("description", forecastAndImgArr[0] );
-                     //Log.i("image", forecastAndImgArr[1] );
+                    //Log.i("description", forecastAndImgArr[0] );
+                    //Log.i("image", forecastAndImgArr[1] );
 
                 }
 
                 //populate listView with forecast data
-                forecastListView.setAdapter(new forecastAdapter(forecastDetails.toArray(new String[0]),toIntArray(forecastImages)));
+                forecastListView.setAdapter(new forecastAdapter(forecastDetails.toArray(new String[0]), toIntArray(forecastImages)));
 
+                //get UV index for location
                 new getUVIndexAsync().execute();
 
             }
@@ -274,9 +294,9 @@ public class WeatherActivity extends AppCompatActivity {
     /**
      * GET request to the weather API, returns JSON.
      *
-     * @param aUrl
+     * @param aUrl api get request url
      * @return weather JSON
-     * @throws IOException
+     * @throws IOException in case the url is incorrect
      */
     private String fetchForecastJSON(String aUrl) throws IOException {
         InputStream is = null;
@@ -306,7 +326,7 @@ public class WeatherActivity extends AppCompatActivity {
             throw e;
         } finally {
             /*
-			 * Make sure that the InputStream is closed after the app is
+             * Make sure that the InputStream is closed after the app is
 			 * finished using it.
 			 * Make sure the connection is closed after the app is finished using it.
 			 */
@@ -327,9 +347,9 @@ public class WeatherActivity extends AppCompatActivity {
     /**
      * Converts the input stream returned form the website into a readable JSON string.
      *
-     * @param stream
+     * @param stream data from api
      * @return JSON String
-     * @throws IOException
+     * @throws IOException in case json is invalid
      */
     public String convertInputStreamToString(InputStream stream) throws IOException {
 
@@ -353,39 +373,39 @@ public class WeatherActivity extends AppCompatActivity {
             JSONObject jsonObj = new JSONObject(weatherDataJson);
 
             //retrieve the number of forecasts "cnt"(count) provided by the API
-            int forecasts =  jsonObj.getInt("cnt");
+            int forecasts = jsonObj.getInt("cnt");
             //retrieve longitude and longitude
             String latitude = Double.toString(jsonObj.getJSONObject("city").getJSONObject("coord").getDouble("lat"));
             String longitude = Double.toString(jsonObj.getJSONObject("city").getJSONObject("coord").getDouble("lon"));
-            sb.append(latitude+"?"+longitude + "#");
+            sb.append(latitude + "?" + longitude + "#");
 
             //get the all the forecasts
             JSONArray forecastList = jsonObj.getJSONArray("list");
 
             //iterate through each forecast and retrieve relevant data
-            for(int forecast=0; forecast < forecasts; forecast++){
+            for (int forecast = 0; forecast < forecasts; forecast++) {
 
                 JSONObject weatherObj = (JSONObject) forecastList.get(forecast);
                 JSONObject weatherTemperature = weatherObj.getJSONObject("main");
                 JSONArray weatherDetails = weatherObj.getJSONArray("weather");
 
                 String forecastDate = weatherObj.getString("dt_txt");
-                String description =  weatherDetails.getJSONObject(0).getString("description");
+                String description = weatherDetails.getJSONObject(0).getString("description");
                 String icon = weatherDetails.getJSONObject(0).getString("icon");
-                String temperature = Integer.toString( (int)Math.round(weatherTemperature.getDouble("temp") - KELVIN)) + "°C";
+                String temperature = Integer.toString((int) Math.round(weatherTemperature.getDouble("temp") - KELVIN)) + "°C";
 
-                description  = description.substring(0, 1).toUpperCase() + description.substring(1);
+                description = description.substring(0, 1).toUpperCase() + description.substring(1);
 
                 //format found in json
-                SimpleDateFormat apiFormat =new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+                SimpleDateFormat apiFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
                 Date parsedDate = apiFormat.parse(forecastDate);
                 //desired format
-                SimpleDateFormat appFormat= new SimpleDateFormat("EEEE MMMM dd ha");
+                SimpleDateFormat appFormat = new SimpleDateFormat("EEEE MMMM dd ha");
 
                 //Log.i("forecast", appFormat.format(parsedDate)+ " " + description + " " +  temperature + " @"+ icon);
 
                 // amalgamate data to be deciphered in async task post execute, "^" separates each weather forecast
-                sb.append(appFormat.format(parsedDate)+ " " + description + " " +  temperature + " @"+ icon + "|");
+                sb.append(appFormat.format(parsedDate) + " " + description + " " + temperature + " @" + icon + "|");
 
             }
 
@@ -397,7 +417,7 @@ public class WeatherActivity extends AppCompatActivity {
     }
 
     /**
-     * This Async task gets the forecast via an open weather API.
+     * This Async task gets the uv forecast via an open weather API.
      * It receives JSON data which is deciphered and displayed to the user.
      */
     private class getUVIndexAsync extends AsyncTask<String, Void, String> {
@@ -406,7 +426,7 @@ public class WeatherActivity extends AppCompatActivity {
         protected String doInBackground(String... location) {
             try {
 
-                String url = WEATHER_UV_URL + "appid=" + API_KEY + "&lat=" + latitude + "&lon=" + longitude ;
+                String url = WEATHER_UV_URL + "appid=" + API_KEY + "&lat=" + latitude + "&lon=" + longitude;
 
                 return fetchUVIndexJSON(url);
 
@@ -420,11 +440,10 @@ public class WeatherActivity extends AppCompatActivity {
 
         protected void onPostExecute(String result) {
 
-            if(result == "Invalid request."){
+            if (result.equalsIgnoreCase("Invalid request.")) {
                 Toast.makeText(getApplicationContext(), "No results found.", Toast.LENGTH_LONG).show();
-            }else {
-
-
+            } else {
+                uvTV.setText(result);
             }
 
 
@@ -508,10 +527,38 @@ public class WeatherActivity extends AppCompatActivity {
         //reset string builder
         sb.setLength(0);
 
+        sb.append("UV Forecast \n");
+
         try {
 
             //parse json text
-            //JSONObject jsonObj = new JSONObject(uvDataJson);
+            JSONArray uvForecasts = new JSONArray(uvDataJson);
+
+            for (int uvForecast = 0; uvForecast < uvForecasts.length(); uvForecast++) {
+                JSONObject forecast = uvForecasts.getJSONObject(uvForecast);
+                int timestamp = forecast.getInt("date");
+                double uvIndex = forecast.getInt("value");
+                String intensity = "LOW";
+
+                Date d = new Date((long) timestamp * 1000);
+                DateFormat f = new SimpleDateFormat("EEEE MMMM dd");
+                f.format(d);
+
+                if (uvIndex > 2 && uvIndex < 6) {
+                    intensity = "MODERATE";
+                } else if (uvIndex >= 6 && uvIndex < 8) {
+                    intensity = "HIGH";
+                } else if (uvIndex >= 8 && uvIndex < 11) {
+                    intensity = "VERY HIGH";
+                }
+                if (uvIndex >= 11) {
+                    intensity = "EXTREME";
+                }
+
+
+                sb.append(f.format(d) + "---" + uvIndex + " " + intensity + " \n");
+
+            }
 
         } catch (Exception e) {
             e.printStackTrace();
@@ -521,17 +568,13 @@ public class WeatherActivity extends AppCompatActivity {
     }
 
 
-
-
-
-
     class forecastAdapter extends BaseAdapter {
         String[] text;
         int[] image;
 
         forecastAdapter() {
             text = null;
-            image =null;
+            image = null;
         }
 
         public forecastAdapter(String[] texts, int[] images) {
@@ -541,15 +584,15 @@ public class WeatherActivity extends AppCompatActivity {
         }
 
         public int getCount() {
-             return text.length;
+            return text.length;
         }
 
         public Object getItem(int arg0) {
-             return null;
+            return null;
         }
 
         public long getItemId(int position) {
-             return position;
+            return position;
         }
 
         public View getView(int position, View convertView, ViewGroup parent) {
@@ -562,7 +605,7 @@ public class WeatherActivity extends AppCompatActivity {
             ImageView i1;
 
             textV = (TextView) row.findViewById(R.id.forecastText);
-            i1=(ImageView)row.findViewById(R.id.forecastImage);
+            i1 = (ImageView) row.findViewById(R.id.forecastImage);
             textV.setText(text[position]);
             i1.setImageResource(image[position]);
 
@@ -570,9 +613,9 @@ public class WeatherActivity extends AppCompatActivity {
         }
     }
 
-    public int[] toIntArray(List<Integer> list){
+    public int[] toIntArray(List<Integer> list) {
         int[] ret = new int[list.size()];
-        for(int i = 0;i < ret.length;i++)
+        for (int i = 0; i < ret.length; i++)
             ret[i] = list.get(i);
         return ret;
     }
